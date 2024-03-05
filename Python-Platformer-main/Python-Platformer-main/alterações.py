e0ed76e16fa42a9a02f9ca15a79ad91daed08b88
fagpaf <fagpaf@cin.ufpe.br>
2024-03-05 11:52:23 -0300

    criando o player

--- Python-Platformer-main/Python-Platformer-main/alterações.py
+++ Python-Platformer-main/Python-Platformer-main/alterações.py
@@ -14,6 +14,39 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Criando o personagem
+class Player(pygame.sprite.Sprite): # Usando herança de Sprite's para facilitar a colisão entre os pixels do jogador com os blocos
+    
+    # Aqui a altura e largura serão determinadas pela imagem q estamos usando para o nosso personagem
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height) # Adicionado todos esses valores em retângulo fica mais fácil de acessar e resolver os problemas das colisões
+        self.x_vel = 0
+        self.y_vel = 0
+        # Pesquisem sobre dps:
+        self.mask = None #Armazena a máscara de colisão correspondente à imagem do objeto, que é usada para detecção de colisão mais precisa
+        
+        self.animation_count = 0 # sem isso vc redefine a animação em quanto o palyer está se movendo e vai bugar a tela
+
+    # Apenas a direção de movimento 
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+    
+    def move_left(self, vel):
+        self.x_vel = -vel # a velocidade é negativa, pq se vc quiser ir para trás estará removendo os quadros relativo a tela do jogo, recomendo verem essa parte do vídeo e uma representação dos eixos no pygame
+    
+    # O resto é entendível aq
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+
 # Criando o fundo do jogo
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name)) # Acessando a pasta que contém a imagem que usaremos de fundo
@@ -39,7 +72,7 @@ def draw(window, background, bg_image):
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image = get_background("Gray.png")
+    background, bg_image = get_background("Purple.png") # bg_image é a imagem de fundo 
 
     run = True
     while run:
