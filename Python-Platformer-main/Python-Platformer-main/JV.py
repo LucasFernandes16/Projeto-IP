ed6d50ed1db555e6c2882fca5a3df57c5dbb73ec
fagpaf <fagpaf@cin.ufpe.br>
2024-03-07 22:04:01 -0300

    Update JV.py

--- Python-Platformer-main/Python-Platformer-main/JV.py
+++ Python-Platformer-main/Python-Platformer-main/JV.py
@@ -1,5 +1,5 @@
 #função responsável por mover nosso personagem principal na tela 
-def move(player):
+def handle_move(player):
     keys = pygame.key.get_pressed() #informa todas as teclas que estão sendo pressionadas no comento 
     player.x_vel = 0 
     #criando a responsividade para apertar a tecla e mover o personagem
