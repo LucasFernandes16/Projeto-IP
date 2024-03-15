6d80c94ea12cadde17c742d348cbb4917f1fc90b
fagpaf <fagpaf@cin.ufpe.br>
2024-03-14 21:48:01 -0300

    mudando

--- Python-Platformer-main/Python-Platformer-main/Flavio.py
+++ Python-Platformer-main/Python-Platformer-main/Flavio.py
@@ -1,4 +1,10 @@
+import os
+import random
+import math
 import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
 
 def get_block(size):
     path = join("assets", "Terrain", "Terrain.png") # Acessa a pasta q contém a imagem do bloco
@@ -13,6 +19,40 @@ def get_block(size):
 
     surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
+
+def flip(sprites):
+    # Função que inverte as imagens horizontalmente
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    # dir1 é a pasta onde as sprites estão guardadas 
+    # dir2 é a sprite q vc quer usar
+    
+    # Função para carregar spritesheets e criar sprites a partir delas
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}  # Dicionário para armazenar todos os sprites
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            # Se houver direção, cria sprites para direita e esquerda
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)  # Chama a função flip para inverter os sprites
+        else:
+            all_sprites[image.replace(".png", "")] = sprites  # Adiciona os sprites sem direção especificada ao dicionário
+
+    return all_sprites  # Retorna o dicionário contendo todos os sprites
+
 
 # Criando o personagem
 class Player(pygame.sprite.Sprite): # Usando herança de Sprite's para facilitar a colisão entre os pixels do jogador com os blocos
@@ -86,6 +126,7 @@ class Player(pygame.sprite.Sprite): # Usando herança de Sprite's para facilitar
     def draw(self, win):
         self.sprite = self.SPRITES["idle_" + self.direction][0]
         win.blit(self.sprite, (self.rect.x, self.rect.y))
+
 
 # Apenas definindo a classe de objetos para usar herença nos outros objetos q iremos criar no jogo
 class Object(pygame.sprite.Sprite):
