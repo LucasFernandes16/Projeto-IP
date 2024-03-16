f14b2aaba5d6123b10f8e1ee006434f7d7c4e446
fagpaf <fagpaf@cin.ufpe.br>
2024-03-16 09:25:34 -0300

    modularinzando

---
+++ Python-Platformer-main/Python-Platformer-main/Objects.py
@@ -0,0 +1,12 @@
+# Apenas definindo a classe de objetos para usar herença nos outros objetos q iremos criar no jogo
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+
+    def draw(self, win, offset_x):
+        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
