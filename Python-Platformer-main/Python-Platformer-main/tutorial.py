b49401464cb710d57c9fbbc9642f13eb358db63c
fagpaf <fagpaf@cin.ufpe.br>
2024-03-19 11:50:56 -0300

    é

--- Python-Platformer-main/Python-Platformer-main/tutorial.py
+++ Python-Platformer-main/Python-Platformer-main/tutorial.py
@@ -145,38 +145,6 @@ class Player(pygame.sprite.Sprite):
         win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
 
-class Collectible(Object):
-    ANIMATION_DELAY = 3
-    
-    def __init__(self, x, y, width, height, name):
-        super().__init__(x, y, width, height, name)
-        self.collectible = load_sprite_sheets("Items", "Fruits", width, height)
-        self.image = self.collectible[name][0]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.animation_count = 0
-        self.animation_name = name
-        self.hit = False
-
-    def hit_collectible(self):
-        self.hit = True
-        
-    def loop(self):
-        if self.hit:
-            self.image = self.collectible["Collected"][0]
-        
-        sprites = self.collectible[self.animation_name] # Obtém os sprites correspondentes à sprite sheet atual
-        
-        sprite_index = (self.animation_count //
-                        self.ANIMATION_DELAY) % len(sprites) # Calcula o índice do sprite a ser exibido com base no atraso entre as animações
-        
-        self.sprite = sprites[sprite_index]  # Define o sprite atual
-        self.animation_count += 1 # Incrementa o contador de animação
-        self.update() # Chama a função de atualização
-        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y)) # Atualiza a posição do retângulo do sprite
-        self.mask = pygame.mask.from_surface(self.sprite) # Atualiza a colisão do sprite
-        
-        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
-            self.animation_count = 0
 
 
 
@@ -294,7 +262,6 @@ def main(window):
 
     block_size = 96
 
-    
     player = Player(100, 100, 50, 50)
     fire = Fire(100, HEIGHT - block_size - 64, 16, 32)
     fire.on()
